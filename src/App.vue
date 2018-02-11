673ebb19c1e803c474d9fdafd6d29663e537412e
dongdongjie <zdj@ourstu.com>
2018-02-11 15:30:45 +0800

    【新增】【调整】新增服务器端获取拼单列表，新增拼单列表部分的滚动效果

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,26 @@
         <el-row>
             <el-col :span='5'>
                 <div class="fa-list" ref="faList">
-                    <transition-group
-                        appear
-                        tag="ul"
-                        :css="false"
-                        @beforeEnter="faItemBeforeEnter"
-                        @enter="faItemEnter"
-                    >
-                        
-                        <li v-for="(item,index) in falistState" class="fa-item" :key="item.id" :data-index="index" :class="falistClass[item.state]">
-                            <router-link :to="{ name: 'fades', params: { id: item.id }}">
-                            <div class="fa-name">{{item.name}}</div>
-                            <div class="fa-user">发起人：{{item.user}}</div>
-                            <div class="fa-time">{{item.time}}</div>   
-                            <div class="fa-state">{{item.stateText}}</div>
-                            </router-link>
-                        </li>
-                        
-                    </transition-group>
+                    <div class="wrapper-div wrapper" ref="wrapperDiv">
+                        <transition-group
+                            appear
+                            tag="ul"
+                            class="content"
+                            :css="false"
+                            @beforeEnter="faItemBeforeEnter"
+                            @enter="faItemEnter"
+                        >
+                            <li v-for="(item,index) in falistState" class="fa-item" :key="item.id" :data-index="index" :class="falistClass[item.state]">
+                                <router-link :to="{ name: 'fades', params: { id: item.id }}">
+                                <div class="fa-name">{{item.name}}</div>
+                                <div class="fa-user">发起人：{{item.user.name}}</div>
+                                <div class="fa-time">{{item.start_time}}</div>  
+                                <div class="fa-time">至{{item.end_time}}</div>  
+                                <div class="fa-state">{{item.stateText}}</div>
+                                </router-link>
+                            </li>
+                        </transition-group>
+                    </div>
                 </div>
             </el-col>
             <!--商品展示-->
@@ -33,66 +35,50 @@
 
 <script type="text/ecmascript-6">
     import  Velocity from 'velocity-animate'
+    import BScroll from 'better-scroll'
     export default {
         name: 'App',
         data() {
             return {
                 faList: [
-                    {
-                        id: 1,
-                        name: "胡来烧饼",
-                        user: "洞洞杰",
-                        time: "2018-2-3 14:00",
-                        state: 0
-                    },
-                    {
-                        id: 2,
-                        name: "黄焖鸡",
-                        user: "通哥",
-                        time: "2018-2-4 15:00",
-                        state: 1
-                    },
-                    {
-                        id: 3,
-                        name: "老鸭煲",
-                        user: "飞哥",
-                        time: "2018-2-3 16:00",
-                        state: 1
-                    },
-                    {
-                        id: 4,
-                        name: "老鸭煲",
-                        user: "飞哥",
-                        time: "2018-2-3 16:00",
-                        state: 1
-                    },
-                    {
-                        id: 5,
-                        name: "老鸭煲",
-                        user: "飞哥",
-                        time: "2018-2-3 16:00",
-                        state: 1
-                    },
+                    
                 ]
             }
         },
         created(){
             console.log("左侧拼单列表创建完毕");
             this.falistClass = ['stop','conduct'];
-            //默认进入第一个拼单item的路由
-            this.$router.push({name: 'fades',params:{id:this.falistState[0].id}});
+            this.IS_INIT = true;
 
-            this.$axios.get(process.env.API_HOST+'zhou').then(response => {
-                console.log(response.data);
+            this.$axios.get(process.env.API_HOST+'faList').then(response => {
+                if(response.data.code == 200){
+                    this.faList = response.data.data;
+                }
+                
             })
             .catch(error=>{
                 console.log(error);
                 alert('网络错误，不能访问');
+            });
+            this.$nextTick(() => {
+                this._initScroll();
             })
+            
+
         },
         mounted() {
             //设置faList的高度为浏览器高度
             this.$refs.faList.style.height = document.body.clientHeight + "px";
+           
+            this.$refs.wrapperDiv.style.height = "100%";
+        },
+        beforeUpdate() {
+            //默认进入第一个拼单item的路由
+            if(this.IS_INIT){
+                this.$router.push({name: 'fades',params:{id:this.falistState[0].id}});
+                this.IS_INIT = false;
+            }
+            
         },
         computed:{
             falistState(){
@@ -109,6 +95,14 @@
             }
         },
         methods: {
+            _initScroll() {
+                this.faist = new BScroll(this.$refs.wrapperDiv,{
+                    click: true
+                })
+            },
+
+
+
             // 过渡进入
             // 设置过渡进入之前的组件状态
             faItemBeforeEnter: function (el) {
@@ -147,44 +141,46 @@
             padding: 20px
             background-color: #eee
             box-shadow: 1px 0 8px 1px rgba(0,0,0,0.2)
-            .fa-item
-                position: relative
-                box-sizing: border-box
-                width: 100%
-                margin: 15px 0
-                padding: 10px
-                border-radius: 5px
+            .wrapper-div
                 overflow: hidden
-                cursor: pointer
-                &.conduct
-                    background: linear-gradient(left,#097cf4,#409eff)
-                    color: #fff
-                &.stop
-                    background: linear-gradient(left,#666,#ccc)
-                    color: #fff
-                &:hover
-                    transform: translateX(10px)
-                a
-                    text-decoration: none
-                    color: #fff
-                    .fa-name
-                        font-size: 18px
-                    .fa-user
-                        margin: 5px 10px
-                        font-size: 14px
-                    .fa-time
-                        text-align: right
-                        font-size: 12px
-                    .fa-state
-                        position: absolute
-                        right: -20px
-                        top: 2px
-                        transform: rotate(30deg)
-                        width: 100px
-                        height: 25px
-                        background: red
-                        box-shadow: 0 0 10px 1px rgba(0,0,0,0.5)
-                        text-align: center
-                        line-height: 25px
-                        font-size: 12px
+                .fa-item
+                    position: relative
+                    box-sizing: border-box
+                    width: 100%
+                    margin-bottom: 15px
+                    padding: 10px
+                    border-radius: 5px
+                    overflow: hidden
+                    cursor: pointer
+                    &.conduct
+                        background: linear-gradient(left,#097cf4,#409eff)
+                        color: #fff
+                    &.stop
+                        background: linear-gradient(left,#666,#ccc)
+                        color: #fff
+                    &:hover
+                        transform: translateX(10px)
+                    a
+                        text-decoration: none
+                        color: #fff
+                        .fa-name
+                            font-size: 18px
+                        .fa-user
+                            margin: 5px 10px
+                            font-size: 14px
+                        .fa-time
+                            text-align: right
+                            font-size: 12px
+                        .fa-state
+                            position: absolute
+                            right: -20px
+                            top: 2px
+                            transform: rotate(30deg)
+                            width: 100px
+                            height: 25px
+                            background: red
+                            box-shadow: 0 0 10px 1px rgba(0,0,0,0.5)
+                            text-align: center
+                            line-height: 25px
+                            font-size: 12px
 </style>
